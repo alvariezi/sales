590c799a1ebf5fa962954b3ed1ce920bcc8eb4f4
alvariezi <148828942+alvariezi@users.noreply.github.com>
2024-08-14 10:30:52 +0700

    add tambah barang pop up

--- lib/components/add_item_dialog.dart
+++ lib/components/add_item_dialog.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 class AddItemDialog extends StatelessWidget {
   final TextEditingController namaController;
@@ -11,6 +14,40 @@ class AddItemDialog extends StatelessWidget {
     required this.kodeController,
     required this.onConfirm,
   }) : super(key: key);
+
+  Future<void> _submitData(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? token = prefs.getString('token');
+    
+    if (token != null) {
+      final url = 'https://backend-sales-pearl.vercel.app/api/owner/inventory';
+      
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token',
+        },
+        body: jsonEncode({
+          'kode_produk': kodeController.text,
+          'nama_produk': namaController.text,
+        }),
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        onConfirm();
+        Navigator.pop(context);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to add item: ${response.statusCode}')),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('No token found')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,8 +79,7 @@ class AddItemDialog extends StatelessWidget {
         ),
         ElevatedButton(
           onPressed: () {
-            onConfirm();
-            Navigator.pop(context);
+            _submitData(context);
           },
           child: const Text('Tambah'),
         ),
