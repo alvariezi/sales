b8db307c6022cb88032807664b0b53151c24c36f
wildandaffa15 <161112149+wildandaffa15@users.noreply.github.com>
2024-08-14 22:41:20 +0700

    update restock screen

--- lib/screens/add_stock.dart
+++ lib/screens/add_stock.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 import 'package:sales/components/input_data_popup.dart';
 
 class AddStockPage extends StatefulWidget {
@@ -12,15 +15,6 @@ class _AddStockPageState extends State<AddStockPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final List<Map<String, dynamic>> _formData = [];
 
-  // List of product names for the dropdown
-  final List<String> productOptions = [
-    'Kecap',
-    'Gula',
-    'Garam',
-    'Saos',
-    'Masako',
-  ];
-
   @override
   void initState() {
     super.initState();
@@ -31,42 +25,65 @@ class _AddStockPageState extends State<AddStockPage> {
   void _addForm() {
     setState(() {
       _formData.add({
-        'selectedProduct': null,
-        'qtyGudangController': TextEditingController(),
+        'namaProdukController': TextEditingController(),
+        'qtyController': TextEditingController(),
       });
     });
   }
 
   void _removeForm(int index) {
     setState(() {
-      _formData[index]['qtyGudangController'].dispose();
+      _formData[index]['namaProdukController'].dispose();
+      _formData[index]['qtyController'].dispose();
       _formData.removeAt(index);
     });
   }
 
-  void addStock() async {
+  Future<void> addStock() async {
     final form = _formKey.currentState;
     if (form != null && form.validate()) {
-      // Handle stock addition logic here
-      _formData.forEach((data) {
-        print('Nama Produk: ${data['selectedProduct']}');
-        print('Qty Gudang: ${data['qtyGudangController'].text}');
-      });
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      final String? token = prefs.getString('token');
 
-      // Message popup input
-      final result = await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return const CustomDialog(
-            title: 'Konfirmasi tambah',
-            message: 'Apakah data yang anda tambahkan benar?',
+      if (token != null) {
+        final url = 'https://backend-sales-pearl.vercel.app/api/owner/restock';
+        final List<Map<String, dynamic>> products = _formData.map((data) {
+          return {
+            'nama_produk': data['namaProdukController'].text,
+            'qty': int.parse(data['qtyController'].text),
+          };
+        }).toList();
+
+        final response = await http.post(
+          Uri.parse(url),
+          headers: {
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'application/json',
+          },
+          body: json.encode({
+            'products': products,
+          }),
+        );
+
+        if (response.statusCode == 201) {
+          final result = await showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return const CustomDialog(
+                title: 'Konfirmasi tambah',
+                message: 'Apakah data yang anda tambahkan benar?',
+              );
+            },
           );
-        },
-      );
 
-      if (result == true) {
-        // Confirm was pressed
-        Navigator.pop(context);
+          if (result == true) {
+            // Navigate back after successful addition
+            Navigator.pop(context);
+          }
+        } else {
+          // Handle error response
+          print('Failed to add stock: ${response.body}');
+        }
       }
     }
   }
@@ -74,7 +91,8 @@ class _AddStockPageState extends State<AddStockPage> {
   @override
   void dispose() {
     _formData.forEach((data) {
-      data['qtyGudangController'].dispose();
+      data['namaProdukController'].dispose();
+      data['qtyController'].dispose();
     });
     super.dispose();
   }
@@ -103,51 +121,44 @@ class _AddStockPageState extends State<AddStockPage> {
                           children: [
                             Expanded(
                               flex: 2,
-                              child: DropdownButtonFormField<String>(
-                                value: data['selectedProduct'],
-                                items: productOptions.map((String product) {
-                                  return DropdownMenuItem<String>(
-                                    value: product,
-                                    child: Text(product),
-                                  );
-                                }).toList(),
-                                onChanged: (String? newValue) {
-                                  setState(() {
-                                    data['selectedProduct'] = newValue;
-                                  });
-                                },
-                                decoration: InputDecoration(
+                              child: TextFormField(
+                                controller: data['namaProdukController'],
+                                decoration: const InputDecoration(
                                   labelText: 'Nama Produk',
                                   border: OutlineInputBorder(),
                                 ),
                                 validator: (value) {
-                                  return value == null ? 'Produk harus terisi' : null;
+                                  return value == null || value.isEmpty
+                                      ? 'Nama produk harus terisi'
+                                      : null;
                                 },
                               ),
                             ),
-                            SizedBox(width: 16.0),
+                            const SizedBox(width: 16.0),
                             Expanded(
                               flex: 1,
                               child: TextFormField(
-                                controller: data['qtyGudangController'],
-                                decoration: InputDecoration(
-                                  labelText: 'Qty Gudang',
+                                controller: data['qtyController'],
+                                decoration: const InputDecoration(
+                                  labelText: 'Qty',
                                   border: OutlineInputBorder(),
                                 ),
                                 keyboardType: TextInputType.number,
                                 validator: (value) {
-                                  return value == null || value.isEmpty ? 'Masukkan jumlah' : null;
+                                  return value == null || value.isEmpty
+                                      ? 'Masukkan jumlah'
+                                      : null;
                                 },
                               ),
                             ),
                             if (index != 0)
                               IconButton(
-                                icon: Icon(Icons.remove_circle, color: Colors.red),
+                                icon: const Icon(Icons.remove_circle, color: Colors.red),
                                 onPressed: () => _removeForm(index),
                               ),
                           ],
                         ),
-                        SizedBox(height: 16.0),
+                        const SizedBox(height: 16.0),
                       ],
                     );
                   },
@@ -157,12 +168,12 @@ class _AddStockPageState extends State<AddStockPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   IconButton(
-                    icon: Icon(Icons.add_circle, color: Colors.blue, size: 30),
+                    icon: const Icon(Icons.add_circle, color: Colors.blue, size: 30),
                     onPressed: _addForm,
                   ),
                 ],
               ),
-              SizedBox(height: 16.0),
+              const SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: addStock,
                 child: const Text('Tambah Stock'),
